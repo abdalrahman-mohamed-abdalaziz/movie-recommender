4d696ffaf89e222d2820489d7f7bcc89b069a543
abdalrahman-mohamed-abdalaziz <abdalrahman.mohamed.work1@gmail.com>
2026-08-15 17:28:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ titles = sorted(data_model["title"].unique().tolist())
 
 def search_movies(searchterm: str):
     if not searchterm:
-        return titles[:50]
+        return []
     return [t for t in titles if searchterm.lower() in t.lower()][:20]
 
 selected_movie = st_searchbox(
